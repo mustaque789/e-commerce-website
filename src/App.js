85cc1c460641bd22c735ce5c2388a22da6b6fc74
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-03-26 12:39:21 -0400

    browserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
 
 const App = () => {
-  return <div>Welcome to Sahil's React E-Commerce Website</div>;
+  return <Router>
+    <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/products" element={<Products/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/singleproduct" element={<SingleProduct/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+    </Routes>
+  </Router>
+
+
 };
 
 export default App;
